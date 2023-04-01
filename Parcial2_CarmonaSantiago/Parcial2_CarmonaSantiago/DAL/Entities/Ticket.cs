271807c9d190c759edc7921e0eed77264205592d
SCarmona02 <santiago.car2003@hotmail.com>
2023-04-01 17:20:56 -0500

    MVC App created in EntityFramework and new Entity Ticket created

---
+++ Parcial2_CarmonaSantiago/Parcial2_CarmonaSantiago/DAL/Entities/Ticket.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2_CarmonaSantiago.DAL.Entities
+{
+    public class Ticket
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Fecha de uso")]
+        public DateTime? UseDate { get; set; }
+
+        [Display(Name = "¿Boleta usada?")]
+        public bool IsUsed { get; set; }
+
+        [Display(Name = "Portería de ingreso")]
+        public string? EntranceGate { get; set; }
+    }
+}
---
+++ Parcial2_CarmonaSantiago/Parcial2_CarmonaSantiago/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Parcial2_CarmonaSantiago.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
