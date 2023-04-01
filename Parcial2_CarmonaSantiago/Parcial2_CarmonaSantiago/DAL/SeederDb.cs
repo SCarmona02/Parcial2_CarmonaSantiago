ca483e65c8cb8ca6938308b2939578801437e2d0
SCarmona02 <santiago.car2003@hotmail.com>
2023-04-01 18:11:22 -0500

    New SeederDb to populate Tickets table

---
+++ Parcial2_CarmonaSantiago/Parcial2_CarmonaSantiago/DAL/SeederDb.cs
@@ -0,0 +1,33 @@
+﻿using Parcial2_CarmonaSantiago.DAL.Entities;
+
+namespace Parcial2_CarmonaSantiago.DAL
+{
+    public class SeederDb
+    {
+        private readonly DatabaseContext _context;
+
+        public SeederDb(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeederAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await PopulateTicketsAsync();
+
+            await _context.SaveChangesAsync();
+        }
+
+        private async Task PopulateTicketsAsync()
+        {
+            if (!_context.Tickets.Any())
+            {
+                for (int i = 1; i <= 50000; i++)
+                {
+                    _context.Tickets.Add(new Ticket { IsUsed = false });
+                }
+            }
+        }
+    }
+}
