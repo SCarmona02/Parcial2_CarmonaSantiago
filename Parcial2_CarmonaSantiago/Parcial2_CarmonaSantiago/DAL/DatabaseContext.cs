ed9318f951cff357b391fcadee6a707dc394e302
SCarmona02 <santiago.car2003@hotmail.com>
2023-04-01 17:46:12 -0500

    New DatabaseContext and DataBase ConcertDB created

---
+++ Parcial2_CarmonaSantiago/Parcial2_CarmonaSantiago/DAL/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial2_CarmonaSantiago.DAL.Entities;
+
+namespace Parcial2_CarmonaSantiago.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
